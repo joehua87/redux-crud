84228211031fac1d73567cccc6418fd139688d35
joehua87 <joehua87@gmail.com>
2016-09-26 20:05:04 +0700

    export reducer default

---
+++ packages/redux-immutable-crud-reducers/src/index.js
@@ -0,0 +1,6 @@
+export createQueryReducer from './create-query-reducer'
+export createEditableReducer from './create-editable-reducer'
+export {
+  rawInitialState,
+  initialState,
+} from './initial-state'
